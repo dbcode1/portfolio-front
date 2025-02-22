d42785acc2abe64b3c7af3318951ec5a084ee7cb
daniel brusky <dmbrusky@gmail.com>
2025-02-22 14:28:34 -0500

    styling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,13 +3,17 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import { div } from "framer-motion/client";
 
+const regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
 const ContactForm = () => {
+  console.log(regEmail.test("d@gmail.com"));
   const [messageData, setMessageData] = useState("");
+  const [email, setEmail] = useState("");
   const submit = async (e) => {
     e.preventDefault();
 
     try {
-      const params = { message: messageData };
+      const params = { message: messageData, email: email };
       console.log(messageData);
       const response = await axios({
         method: "post",
@@ -26,13 +30,16 @@ const ContactForm = () => {
     console.log(e.target.value);
     setMessageData(e.target.value);
   };
-  console.log("message", messageData);
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div className="form-wrapper">
       <ToastContainer />
       <form className="form">
         <label htmlFor="email">Email</label>
-        <input type="text" className="form__email" />
+        <input type="email" className="form__email" onChange={emailHandler} />
         <label htmlFor="message">Message</label>
         <textarea
           id="message"
@@ -40,10 +47,26 @@ const ContactForm = () => {
           type="text"
           className="form__message"
         />
-        <a href="#" type="button" onClick={submit} className="form__submit">
+        <a href="#" type="submit" onClick={submit} className="form__submit">
           Contact
         </a>
       </form>
+      <div className="contact-wrap">
+        <a href="https://www.linkedin.com/in/daniel-brusky-6a0a7a1b6/">
+          <img
+            src="../../public/assets/linkedin.svg"
+            alt=""
+            className="linkedin"
+          />
+        </a>
+
+        <a className="github-link" href="https://github.com/dbcode1">
+          <img
+            className="github-contact"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg"
+          />
+        </a>
+      </div>
     </div>
   );
 };
